aa98d0eb3c29766309adca518cdd2be3ff3b6c75
rajeevshishodia <41477000+rajeevshishodia@users.noreply.github.com>
2022-09-28 17:37:53 +0530

    Updates

---
+++ precache-manifest.d1a29220966d4a5db737d04c636bd6a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d9cd7858fbf065c57db5",
+    "url": "/TextUtils/static/css/main.151b47d4.chunk.css"
+  },
+  {
+    "revision": "d9cd7858fbf065c57db5",
+    "url": "/TextUtils/static/js/main.d9cd7858.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/TextUtils/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "5b977f20bbc5700a25d3",
+    "url": "/TextUtils/static/js/2.5b977f20.chunk.js"
+  },
+  {
+    "revision": "14506651abc0b1064f2d",
+    "url": "/TextUtils/static/js/runtime~main.14506651.js"
+  },
+  {
+    "revision": "cfc773c4be7b702d6a1d63b45d6729c2",
+    "url": "/TextUtils/index.html"
+  }
+];
